fa255963d70c9af2945d9f551ee1bd7927d765f5
John N. Norman <normanj777@gmail.com>
2021-05-23 07:17:59 -0400

    add menue display toggle to hamburger button

--- js/script.js
+++ js/script.js
@@ -1,10 +1,13 @@
 const hamburger = document.getElementById('hamburger');
+const mobileMenue = document.getElementById('mobile-menue');
 
 const changeBurger = ()=> {
     if(hamburger.classList.contains('change')) {
         hamburger.classList.remove('change');
+        mobileMenue.classList.remove('active');
     } else {
         hamburger.classList.add('change');
+        mobileMenue.classList.add('active');
     }   
 }
 
