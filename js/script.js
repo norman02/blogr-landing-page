b09bf294d5ffa67b8f81ba7ff8bbc6445747382b
John N. Norman <normanj777@gmail.com>
2021-06-03 14:18:43 -0400

    add function to mobile menue

--- js/script.js
+++ js/script.js
@@ -1,6 +1,14 @@
 const burger = document.getElementById('burger')
+const navMenue = document.getElementById('nav')
 
 
 burger.addEventListener('click', ()=> {
     burger.classList.toggle('change')
+    navMenue.classList.toggle('active');
 })
+
+document.querySelectorAll('.collapse').forEach(item => {
+    item.addEventListener('click', event => {
+      item.classList.toggle('open')
+    })
+  })
